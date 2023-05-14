08b0712dbdc39d823fc96792f76514c839876b2c
kilipan <kilipan@tutanota.com>
2023-05-14 13:58:55 +0200

    fixed default and meetup keymaps

--- keymaps/meetup/keymap.c
+++ keymaps/meetup/keymap.c
@@ -18,44 +18,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_QWERTY] = LAYOUT(
                  KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,
-                 KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_X,    KC_C,    KC_B,                      KC_M,    KC_COMM, KC_DOT,  KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,  KC_P,
+                 KC_X,    KC_C,    KC_B,                      KC_M,    KC_COMM, KC_DOT,
                                    NUM_ESC, NAV_SPC, KC_N,    FUN_ENT
     ),
     [_APTMAK] = LAYOUT(
                  KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,
-                 HRM_S,   HRM_T,   HRM_H,   HRM_K,   HRM_X,   HRM_N,   HRM_A,   HRM_I,
-        HRM_R,   KC_C,    KC_G,    KC_D,                      KC_M,    KC_COMM, KC_DOT,  HRM_O,
+        HRM_R,   HRM_S,   HRM_T,   HRM_H,   HRM_K,   HRM_X,   HRM_N,   HRM_A,   HRM_I,  HRM_O,
+                 KC_C,    KC_G,    KC_D,                      KC_M,    KC_COMM, KC_DOT,
                                    NUM_ESC, NAV_SPC, SYM_KCE, FUN_ENT
     ),
     [_COLEMAK] = LAYOUT(
                  KC_W,    KC_F,    KC_P,    KC_B,    KC_K,    KC_L,    KC_U,    KC_Y,
-                 KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,
-        KC_A,    KC_X,    KC_C,    KC_D,                      KC_H,    KC_COMM, KC_DOT,  KC_O,
+        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,  KC_O,
+                 KC_X,    KC_C,    KC_D,                      KC_H,    KC_COMM, KC_DOT,
                                    NUM_ESC, NAV_SPC, SYM_KCE, FUN_ENT
     ),
     [_NUM] = LAYOUT(
                  KC_9,    KC_8,    KC_7,    XXXXXXX,             KC_PLUS, KC_7,    KC_8,    KC_9,
-                 ALT_T(KC_6),GUI_T(KC_5),SFT_T(KC_4), XXXXXXX,   KC_MINS, KC_4,    KC_5,    KC_6,
-        KC_LCTL, KC_3,    KC_2,    KC_1,                                  KC_1,    KC_2,    KC_3,    KC_SLASH,
+        KC_LCTL, ALT_T(KC_6),GUI_T(KC_5),SFT_T(KC_4), XXXXXXX,   KC_MINS, KC_4,    KC_5,    KC_6,    KC_SLASH,
+                 KC_3,    KC_2,    KC_1,                                  KC_1,    KC_2,    KC_3,
                                    _______, _______,             KC_E,    KC_0
     ),
     [_NAV] = LAYOUT(
                  KC_INS,  KC_UP,   KC_BSPC, KC_VOLD,                   KC_VOLU, KC_BSPC, KC_DEL,  KC_INS,
-                 ALT_T(KC_LEFT),GUI_T(KC_DOWN),SFT_T(KC_RGHT),KC_MUTE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-        KC_LCTL, KC_HOME, KC_PGDN, KC_END,                                      KC_HOME, KC_PGUP, KC_END,  KC_TAB,
+        KC_LCTL, ALT_T(KC_LEFT),GUI_T(KC_DOWN),SFT_T(KC_RGHT),KC_MUTE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  KC_TAB,
+                 KC_HOME, KC_PGDN, KC_END,                                      KC_HOME, KC_PGUP, KC_END,
                                    _______, _______,                   _______, _______
     ),
     [_SYM] = LAYOUT(
                  KC_GRV,  KC_DQT,  KC_MDOT, KC_LBRC, KC_RBRC, KC_BSLS, KC_PIPE, KC_QUES,
-                 KC_AT,   KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_AMPR, KC_ASTR, KC_PERC,
-        KC_EXLM, KC_TILD, KC_QUOT, KC_UNDS,                   KC_MINS, KC_PLUS, KC_EQL,  KC_CIRC,
+        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_AMPR, KC_ASTR, KC_PERC,  KC_CIRC,
+                 KC_TILD, KC_QUOT, KC_UNDS,                   KC_MINS, KC_PLUS, KC_EQL,
                                    _______, KC_LSFT, _______, _______
     ),
     [_FUN] = LAYOUT(
                  QK_BOOT, _______, _______, _______, KC_F12,  KC_F7,   KC_F8,   KC_F9,
-                 _______, _______, _______, _______, KC_F11,  KC_F4,   KC_F5,   KC_F6,
-        QK_RBT,  _______, _______, _______,                   KC_F1,   KC_F2,   KC_F3,   KC_F10,
+        QK_RBT,  _______, _______, _______, _______, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_F10,
+                 _______, _______, _______,                   KC_F1,   KC_F2,   KC_F3,
                                    _______, _______, _______, _______
     )
 };
